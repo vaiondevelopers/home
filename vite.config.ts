1b7e4eb9e46330fb45833a7120a89a783fe43525
vaiondevelopers <vaiondevelopers@gmail.com>
2025-06-08 11:55:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Chainreact-Landing-Page/', // 👈 IMPORTANT: use your repo name exactly here
   server: {
     host: "::",
     port: 8080,
